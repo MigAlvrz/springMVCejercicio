9d41199957ccd8e06b236677e9eb5c345933f4b7
Alfredo <alfredocaloto@gmail.com>
2021-08-02 13:51:21 +0200

    listUsersDB.java working, not done yet. Next step is to do an arrayList with the users.

--- src/main/java/testMain.java
+++ src/main/java/testMain.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 import com.capgemini.persistance.CreateDB;
 import com.capgemini.persistance.LoginDB;
+import com.capgemini.persistance.listUsersDB;
 
 public class testMain {
 
@@ -9,6 +10,7 @@ public class testMain {
 		// TODO Auto-generated method stub
 		CreateDB createDB = new CreateDB();
 		LoginDB loginDB = new LoginDB();
+		listUsersDB listUsersDB = new listUsersDB();
 		System.out.println("Creando base de datos...");
 		createDB.crearDB();
 		//login base de datos
@@ -20,7 +22,8 @@ public class testMain {
 		System.out.println("introduzca Contraseña");
 		password = in.next();
 		loginDB.login(username, password);
-		
+		//mostrar usuarios sin contraseña
+		listUsersDB.listUsers();
 	}
 
 }

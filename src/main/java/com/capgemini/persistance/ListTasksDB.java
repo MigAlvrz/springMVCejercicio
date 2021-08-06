b1dcc434197f60eab5a091efa192f5fb947ad424
Alfredo <alfredocaloto@gmail.com>
2021-08-06 14:05:23 +0200

    ListaTareas.java fully tested and working

--- src/main/java/com/capgemini/persistance/ListTasksDB.java
+++ src/main/java/com/capgemini/persistance/ListTasksDB.java
@@ -78,6 +78,12 @@ public class ListTasksDB extends DBConnection{
 		return null;
 	}
 	
+	/*
+	 *  En esta metodo mostramos las tareas de la base de datos cumpliendo con los parametros de "semana".
+	 *  Estos parámetros son:
+	 *  
+	 *  
+	 */
 	public ArrayList<String> listSemana(int UserID){
 		ArrayList <String> semana = new ArrayList<String>();
 		try {
@@ -85,7 +91,7 @@ public class ListTasksDB extends DBConnection{
 			Connection con = super.DBAccess();
 			Statement stmt = con.createStatement();
 			//realiza la consulta
-			ResultSet rs = stmt.executeQuery("SELECT t.comments, t.created, t.finished, t.planned, t.title, t.category_id, t.user_id, c.id,  c.name FROM ttasks t, tcategories c WHERE (planned <= current_date + (INTERVAL '7' DAY)) AND (user_id = 2) AND c.id = t.category_id ORDER BY planned ASC, name");
+			ResultSet rs = stmt.executeQuery("SELECT t.comments, t.created, t.finished, t.planned, t.title, t.category_id, t.user_id, c.id,  c.name FROM ttasks t, tcategories c WHERE (planned <= current_date + (INTERVAL '7' DAY)) AND (user_id = "+UserID+") AND c.id = t.category_id ORDER BY planned ASC, name");
 			while(rs.next()) {
 				String commentsDB = rs.getString("comments");
 				String createdDB = rs.getString("created");
@@ -108,5 +114,41 @@ public class ListTasksDB extends DBConnection{
 		}
 		return null;
 	}
+	/*
+	 *  En esta metodo mostramos las tareas de la base de datos cumpliendo con los parametros de "semana".
+	 *  Estos parámetros son:
+	 *  
+	 *  
+	 */
+	public ArrayList<String> listTareasCategorias(int UserID, String nombreCategoria){
+		ArrayList <String> tareasCategorias = new ArrayList<String>();
+		try {
+			//conexion con la BD
+			Connection con = super.DBAccess();
+			Statement stmt = con.createStatement();
+			//realiza la consulta
+			ResultSet rs = stmt.executeQuery("SELECT t.comments, t.created, t.finished, t.planned, t.title, t.category_id, t.user_id, c.id,  c.name FROM ttasks t, tcategories c WHERE (c.name = '"+nombreCategoria+"') AND (user_id = "+UserID+") AND c.id = t.category_id ORDER BY planned ASC");
+			while(rs.next()) {
+				String commentsDB = rs.getString("comments");
+				String createdDB = rs.getString("created");
+				String plannedDB = rs.getString("planned");
+				String titleDB = rs.getString("title");
+				String categoria = rs.getString("name");
+				tareasCategorias.add(titleDB);
+				tareasCategorias.add(createdDB);
+				tareasCategorias.add(plannedDB);
+				tareasCategorias.add(commentsDB);
+				tareasCategorias.add(categoria);
+			}
+			//cerramos la conexión
+			con.close();
+			return tareasCategorias;
+		}
+		catch (Exception e) {
+			System.out.println("listTasks failed.");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 }
-
